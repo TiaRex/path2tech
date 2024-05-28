5b20a9307031257663b186ab62f8a5236a661659
Tia Truong <tiatruongti@gmail.com>
2024-05-28 18:30:41 -0500

    Completed prac_7-6.js

--- javascript/Lessons/prac_7-6.js
+++ javascript/Lessons/prac_7-6.js
@@ -1,23 +1,15 @@
 // # use the spread operator to combine arrays
-
 const arr1 = [1, 2, 3, 4, 5];
-
 const arr2 = [6, 7, 8, 9, 10];
+const arr3 = [...arr1,...arr2];
 
-const arr3 = [];
-
-console.log(`Result from combining arrays: ${____}`);
+console.log(`Result from combining arrays: ${arr3}`);
 
 // # use the concat method to combine arrays
-
 const arr4 = [1, 2, 3, 4, 5];
-
 const arr5 = [6, 7, 8, 9, 10];
-
 const arr6 = [11, 12, 13, 14, 15];
-
 const arr7 = [16, 17, 18, 19, 20];
+const arr8 = [arr4.concat(arr5,arr6,arr7)];
 
-const arr8 = [];
-
-console.log(`Result from combining arrays: ${____}`);
+console.log(`Result from combining arrays: ${arr8}`);
