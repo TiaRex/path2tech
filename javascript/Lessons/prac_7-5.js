3ae21b654e795b81811b64c8a9d4fc4935f01a7e
Tia Truong <tiatruongti@gmail.com>
2024-05-28 17:44:15 -0500

    Completed prac_7-5.js

--- javascript/Lessons/prac_7-5.js
+++ javascript/Lessons/prac_7-5.js
@@ -1,51 +1,36 @@
 // # use nested loops to iterate through elements in an array
 
 let multiArray = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+];
 
-    [1, 2, 3],
+for(let i = 0; i < 3; i++) {
+    for(let j = 0; j < 3; j++) {
+        console.log(multiArray[i][j]);
+    }
+}
     
+// # use nested loops to iterate through a 3d array and print the elements
+let multiArray3D = [
+    [[1, 2, 3],
     [4, 5, 6],
+    [7, 8, 9],],
     
-    [7, 8, 9],
-    
-    ];
-    
-    // # write code here
-    
-    // # use nested loops to iterate through a 3d array and print the elements
-    
-    let multiArray3D = [
-    
-    [
-    
-    [1, 2, 3],
-    
-    [4, 5, 6],
-    
-    [7, 8, 9],
-    
-    ],
-    
-    [
-    
-    [10, 11, 12],
-    
+    [[10, 11, 12],
     [13, 14, 15],
+    [16, 17, 18],],
     
-    [16, 17, 18],
-    
-    ],
-    
-    [
-    
-    [19, 20, 21],
-    
+    [[19, 20, 21],
     [22, 23, 24],
-    
-    [25, 26, 27],
-    
-    ],
-    
-    ];
-    
-    // # write code here
+    [25, 26, 27],],
+];
+
+for(let i = 0; i < multiArray3D.length; i++) {
+    for(let j = 0; j < multiArray3D[0].length; j++) {
+        for(let k = 0; k < multiArray3D[0][0].length; k++) {
+            console.log(multiArray3D[i][j][k]);
+        }
+    }
+}
