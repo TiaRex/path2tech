026d14b9359dd0f42261cf071a97fb2ba1d14437
Tia Truong <tiatruongti@gmail.com>
2024-06-04 20:00:17 -0500

    Create props

--- react/Projects/weather_application/src/App.js
+++ react/Projects/weather_application/src/App.js
@@ -2,13 +2,32 @@ import './App.css';
 import SearchBar from './components/SearchBar';
 import WeatherData from './components/WeatherData';
 
+const weatherData = [
+  {
+    day: "Monday",
+    temperature: 20,
+    condition: "Sunny"
+  },
+  {
+    day: "Tuesday",
+    temperature: 22,
+    condition: "Cloudy"
+  },
+  {
+    day: "Wednesday",
+    temperature: 18,
+    condition: "Rainy"
+  },
+    // Add more data as needed
+];
+
 function App() {
   return (
     <div id='main' className="container">
       {/* Search Bar Component */}
       <SearchBar/>
       {/* Iterating through array */}
-      <WeatherData/>
+      <WeatherData list={weatherData}/>
     </div>
   );
 }
--- react/Projects/weather_application/src/components/SearchBar.js
+++ react/Projects/weather_application/src/components/SearchBar.js
@@ -1,20 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function SearchBar() {
-    return (
-        <section>
-            <form>
-                <label htmlFor='search-city'>
-                    <input type='text' placeholder='Search City'
-                    id='search-city' name='search-city'
-                    className='search-city'/>
-                </label>
-                <button type='submit' className='submit-search-city'>
-                    Search
-                </button>
-            </form>
-        </section>
-    )
+  const [searchTerm, setSearchTerm] = useState('');
+  function handleChange(event) {
+    setSearchTerm(event.target.value);
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log('Submitting')
+  }
+  return (
+    <section>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='search-city'>
+          <strong>Search Term: {searchTerm} </strong>
+          <br/>
+          <input type='text'
+          placeholder='Search City'
+          id='search-city'
+          name='search-city'
+          className='search-city'
+          onChange={handleChange}
+          />
+        </label>
+        <button type='submit' className='submit-search-city'>
+          Search
+        </button>
+      </form>
+    </section>
+  )
 }
 
 export default SearchBar;
