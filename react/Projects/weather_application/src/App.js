f2e70a5747b4469912ba17606c0634c177610e54
Tia Truong <tiatruongti@gmail.com>
2024-06-25 19:18:38 -0500

    Added Pages and NavBar

--- react/Projects/weather_application/src/App.js
+++ react/Projects/weather_application/src/App.js
@@ -1,55 +1,19 @@
-import { useState, useEffect, useCallback } from 'react';
 import './App.css';
-import SearchBar from './components/SearchBar';
-import WeatherData from './components/WeatherData';
-import { getWeatherData } from './services/WeatherService';
+import { Route, Routes } from 'react-router-dom'
+import Main from './pages/Main';
+import About from './pages/About';
+import NavBar from './components/NavBar';
 
 function App() {
-  const persistedLocation = localStorage.getItem('searchTerm');
-  const [searchTerm, setSearchTerm] = useState(persistedLocation || 'Stockton, CA');
-  const [weatherData, setWeatherData] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-  const [city, setCity] = useState(searchTerm);
-  const fetchDataCallback = useCallback(async() => {
-    try {
-      setLoading(true);
-      const response = await getWeatherData(city);
-      setWeatherData(response.data.list);
-      setLoading(false);
-    } catch {
-      setError(true);
-      setLoading(false);
-    }
-  },[city]);
-  function handleChange(event) {
-    setSearchTerm(event.target.value);
-  }
-  function handleSubmit(event) {
-    event.preventDefault();
-    setCity(searchTerm);
-  }
-  useEffect(() => {
-    localStorage.setItem('searchTerm', searchTerm);
-  }, [searchTerm]);
-  useEffect(() => {
-    fetchDataCallback();
-  }, [fetchDataCallback]);
-  return (
-    <div id='main' className={"container"}>
-      {/* Search Bar Component */}
-      <SearchBar handleSubmit={handleSubmit} searchTerm=
-      {searchTerm} handleChange={handleChange} id={'search-city'}>
-        <strong>Search City: {searchTerm}</strong>
-      </SearchBar>
-      {error && <p>There was an error loading your data.</p>}
-      {/* Iterating through array */}
-      {loading ? <p>Data Loading...</p> : (
-        <WeatherData list={weatherData}/>
-      )}
-      
-    </div>
-  );
+  return(
+    <section>
+    <NavBar />
+    <Routes>
+      <Route path="/" element={<Main />} />
+      <Route path="/about/:id" element={<About />} />
+    </Routes>
+    </section>
+  )
 }
 
 export default App;
