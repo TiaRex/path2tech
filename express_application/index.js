418d4e31c4c219793758dd029def77dd7bb9f31c
Tia Truong <tiatruongti@gmail.com>
2024-07-02 15:14:08 -0500

    Set Up Route

--- express_application/index.js
+++ express_application/index.js
@@ -16,6 +16,12 @@ then(() => {
 })
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
